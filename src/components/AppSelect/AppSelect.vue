a3d84966efc328480f0468720a58b25bd0500081
ildarH <ildar.habibrahmanov@icloud.com>
2024-09-04 21:46:23 +0500

    feat(components): add app select component

---
+++ src/components/AppSelect/AppSelect.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+import type { SelectOption } from '@/@types/components'
+
+interface SelectProps {
+  options: SelectOption
+  placeholder: string
+}
+
+defineProps<SelectProps>()
+</script>
+
+<template>
+  <Select @update:modelValue="$emit($event)">
+    <SelectTrigger title="Select">
+      <SelectValue :placeholder="placeholder" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectItem v-for="(value, key) in options" :value="key" :key="`${key}-select-value`">
+        {{ value }}
+      </SelectItem>
+    </SelectContent>
+  </Select>
+</template>
+
+<style scoped></style>
